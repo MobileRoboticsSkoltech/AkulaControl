10466394cecb149383128db3a17c695da31b5877
Devilox96 <devilox96@yandex.ru>
2021-09-09 17:58:40 +0300

    Added ROS2 package for the Akula robot

---
+++ AkulaPackageROS2/launch/Main.launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import Command
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                get_package_share_directory('velodyne_driver'),
+                '/launch/velodyne_driver_node-VLP16-launch.py'
+            ])
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                get_package_share_directory('velodyne_pointcloud'),
+                '/launch/velodyne_convert_node-VLP16-launch.py'
+            ])
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{
+                'robot_description': Command([
+                    'xacro',
+                    ' ',
+                   get_package_share_directory('AkulaPackage') + '/model/akula.urdf'
+                ])
+            }]
+        ),
+        Node(
+            package='BaslerROS2',
+            executable='BaslerNode',
+            name='Basler'
+        )
+    ])
---
+++ AkulaPackageROS2/src/AkulaMainNode.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+//-----------------------------//
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/temperature.hpp>
+//-----------------------------//
+int main(int argc, char ** argv) {
+  (void) argc;
+  (void) argv;
+
+  printf("hello world AkulaPackage package\n");
+  return 0;
+}
